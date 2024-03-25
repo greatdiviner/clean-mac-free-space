644bd7611540dfbf5c446d0b8050d52a79c3ffb2
Sam Weng <greatdiviner@gmail.com>
2024-03-25 10:39:04 +0800

    Update reset-clion-eval.sh

--- reset-clion-eval.sh
+++ reset-clion-eval.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+rm -rf ~/Library/Application Support/JetBrains/CLion2021.1/eval
+rm -rf ~/Library/Application Support/JetBrains/GoLand2021.1/eval
+rm -rf ~/Library/Application Support/JetBrains/IntelliJIdea2021.1/eval
